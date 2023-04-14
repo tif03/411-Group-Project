a122974bc9912300424e2678528fc26a0eba8cc7
Tiffany Yu <tif03.yu@gmail.com>
2023-04-14 15:19:40 -0400

    moved things around + add flow chart to docs folder

--- app.py
+++ app.py
@@ -1,25 +1,67 @@
 from flask import Flask, render_template, request
 from weather import main as get_weather
 
+from dotenv import load_dotenv
+import os
+import base64
+from requests import post, get
+from logging import error
+import json
+import random
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
 app = Flask("411-Group-Project")
 
-# we need to pass in what methods are allowed to be called from this route, by default only get is allowed
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def home():
-    #initialize data to none so even if its a get method render template can still have something declared for data
-    data = None
-    
-    # when the user clicks the button to send in the form, they make a post request
-    if request.method == 'POST':
-        city = request.form['cityName']
-        state = request.form['stateName']
-        country = request.form['countryName']
-        
-        # we will call our get_weather method we created, save result in data
-        data = get_weather(city, state, country)
-    
-    # we then pass in data as a parameter to the front end so we can display it there
-    return render_template("index.html", data=data)
+    return render_template("index.html")
+
+def generateRandomString(length):
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    return ''.join(random.choice(letters) for i in range(length))
+
+@app.route("/login")
+def login():
+    redirect_uri = app.config['REDIRECT_URI']
+    state = generateRandomString(16)
+    scope = 'ugs-image-upload playlist-modify-private playlist-modify-public user-top-read'
+    params = {
+        "response_type": "code",
+        "client_id": client_id,
+        "scope": scope,
+        "redirect_uri": redirect_uri,
+        "state": state,
+    }
+    url = "https://accounts.spotify.com/authorize?"
+    result = post(url, headers=params)
+    json_result = json.loads(result.content)
+    return json_result
+
+def getToken(code):
+    authorization = app.config['AUTHORIZATION']
+    redirect_uri = app.config['REDIRECT_URI']
+
+    auth_string = client_id + ":" + client_secret
+    auth_bytes =  auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {'Authorization': authorization, 
+             'Accept': 'application/json', 
+             'Content-Type': 'application/x-www-form-urlencoded'}
+    body = {'code': code, 'redirect_uri': redirect_uri, 
+          'grant_type': 'authorization_code'}
+    post_response = post(url,headers=headers,data=body)
+    if post_response.status_code == 200:
+        pr = post_response.json()
+        return pr['access_token'], pr['refresh_token'], pr['expires_in']
+    else:
+        error('getToken:' + str(post_response.status_code))
+        return None
 
 if __name__ == "__main__":
     app.run()
